ac5f3e8b6fa2a84fb5bfdccf6d661d4b2109c746
lucassperanzini <RM98297@fiap.com.br>
2023-09-16 18:58:15 -0300

    responsivo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menu");
+    const nav = document.querySelector(".navegacao");
+    const star = document.querySelector('#star')
+
+    menuIcon.addEventListener("click", function() {
+        if (nav.classList.contains("show-mobile-nav")) {
+            nav.classList.remove("show-mobile-nav");
+        } else {
+            nav.classList.add("show-mobile-nav");
+           
+           
+            
+        }
+    });
+});
